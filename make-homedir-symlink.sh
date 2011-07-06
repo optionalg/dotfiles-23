955ebfb0183ab5c736fb2d369d9211442ec7efa5
yukihiro hara <yukihr@gmail.com>
2011-07-06 09:25:29 +0900

    update

--- make-homedir-symlink.sh
+++ make-homedir-symlink.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ "${HOST}" != "yhmba\.local" ]; then
-  find "${PWD}" -maxdepth 1 -name '.git' -prune -o -name '.gitignore' -prune -o -name '.*' -print0 | xargs -0 ln -fs -t ~/
-else
+if [ `uname -s` = 'Darwin' ]; then
   files=`find "${PWD}" -maxdepth 1 -name '.git' -prune -o -name '.gitignore' -prune -o -name '.*' -print`
 
   for file in ${files}
   do
     ln -fs ${file} ~/
   done
+else
+  find "${PWD}" -maxdepth 1 -name '.git' -prune -o -name '.gitignore' -prune -o -name '.*' -print0 | xargs -0 ln -fs -t ~/
 fi
 
