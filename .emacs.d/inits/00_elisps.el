d8f47885f3216e365bf53493bc068d7a697bbb8d
yukihiro hara <yukihr@gmail.com>
2011-07-23 23:40:14 +0900

    emacs:term

--- .emacs.d/inits/00_elisps.el
+++ .emacs.d/inits/00_elisps.el
@@ -1,3 +1,17 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/elisps")
+
 (require 'auto-save-buffers)
-(run-with-idle-timer 0.5 t 'auto-save-buffers)
+(run-with-idle-timer 5 t 'auto-save-buffers)
+
+(require 'multi-term)
+(setq multi-term-program shell-file-name)
+(global-set-key (kbd "C-c t") '(lambda ()
+                                 (interactive)
+                                 (if (get-buffer "*terminal<1>*")
+                                     (switch-to-buffer "*terminal<1>*")
+                                   (multi-term))))
+(global-set-key (kbd "C-c n") 'multi-term-next)
+(global-set-key (kbd "C-c p") 'multi-term-prev)
+
+(setq term-default-bg-color nil)
+(setq term-default-fg-color nil)
