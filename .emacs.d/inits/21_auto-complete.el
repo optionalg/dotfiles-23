7d9eb640e2f137bd764516fc181ddcd5972d56d0
yukihiro hara <yukihr@gmail.com>
2011-07-24 01:16:32 +0900

    autocomplete:init

---
+++ .emacs.d/inits/21_auto-complete.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/auto-complete")
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/dict")
+(ac-config-default)
