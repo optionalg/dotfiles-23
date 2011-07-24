1beb054866973589195db98f36f579132002f02c
yukihiro hara <yukihr@gmail.com>
2011-07-25 00:29:17 +0900

    emacs: path and font

--- .emacs.d/inits/00_basic.el
+++ .emacs.d/inits/00_basic.el
@@ -118,3 +118,51 @@
 (require 'server)
 (unless (server-running-p)
   (server-start))
+
+;; path
+;; より下に記述した物が PATH の先頭に追加されます
+(dolist (dir (list
+              "/sbin"
+              "/usr/sbin"
+              "/bin"
+              "/usr/bin"
+              "/opt/local/bin"
+              "/sw/bin"
+              "/usr/local/bin"
+              (expand-file-name "~/bin")
+              (expand-file-name "~/.emacs.d/bin")
+              (expand-file-name "~/.nave/installed/0.4.9/bin")
+              ))
+ ;; PATH と exec-path に同じ物を追加します
+ (when (and (file-exists-p dir) (not (member dir exec-path)))
+   (setenv "PATH" (concat dir ":" (getenv "PATH")))
+   (setq exec-path (append (list dir) exec-path))))
+
+;; fonts
+(when (and
+       (>= emacs-major-version 23)
+       (boundp 'window-system)
+       (eq system-type 'darwin))
+  (set-face-attribute 'default nil
+                      :family "monaco"
+                      :height 120)
+  (set-fontset-font
+   (frame-parameter nil 'font)
+   'japanese-jisx0208
+   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+  (set-fontset-font
+   (frame-parameter nil 'font)
+   'japanese-jisx0212
+   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+  (set-fontset-font
+   (frame-parameter nil 'font)
+   'mule-unicode-0100-24ff
+   '("monaco" . "iso10646-1"))
+  (setq face-font-rescale-alist
+        '(("^-apple-hiragino.*" . 1.1)
+          (".*osaka-bold.*" . 1.2)
+          (".*osaka-medium.*" . 1.2)
+          (".*courier-bold-.*-mac-roman" . 1.0)
+          (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+          (".*monaco-bold-.*-mac-roman" . 0.9)
+          ("-cdac$" . 1.3))))
