28637868525a5e19cccd1a4c6bb6e109b5a3705b
yukihiro hara <yukihr@gmail.com>
2011-07-25 15:53:15 +0900

    emacs:colortheme

--- .emacs.d/inits/02_colortheme.el
+++ .emacs.d/inits/02_colortheme.el
@@ -1,8 +1,12 @@
-(add-to-list 'load-path "~/.emacs.d/vendor/color-theme")
-(require 'color-theme)
-(eval-after-load "color-theme"
-                 '(progn
-                    (color-theme-initialize)
-                    ;; (color-theme-dark-laptop)
-                    (color-theme-ld-dark)
-                    ))
+(unless
+    (string-match "^sid" (system-name)) ;; on sid, disable color theme
+
+  (add-to-list 'load-path "~/.emacs.d/vendor/color-theme")
+  (require 'color-theme)
+  (eval-after-load "color-theme"
+    '(progn
+       (color-theme-initialize)
+       ;; (color-theme-dark-laptop)
+       (color-theme-ld-dark)
+       ))
+  )
