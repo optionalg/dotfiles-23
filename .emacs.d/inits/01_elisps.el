c22d54548c04ed9f10db2d8e9d88a2e34f0de8d4
yukihiro hara <yukihr@gmail.com>
2011-08-04 23:51:07 +0900

    emacs: install term-toggle and fix not loaded problem

--- .emacs.d/inits/01_elisps.el
+++ .emacs.d/inits/01_elisps.el
@@ -1,4 +1,5 @@
-(add-to-list 'load-path "~/.emacs.d/vendor/elisps")
+;; 設定されてないように見える・・ → autoload が失敗
+(setq load-path (cons (expand-file-name "~/.emacs.d/vendor/elisps") load-path))
 
 (require 'auto-save-buffers)
 (run-with-idle-timer 5 t 'auto-save-buffers)
@@ -12,4 +13,12 @@
 (setq recentf-max-saved-items 1000)
 (recentf-mode 1)
 
-(autoload 'ac-mode "ac-mode" "Minor mode for advanced completion." t nil)
+(autoload 'ac-mode "~/.emacs.d/vendor/elisps/ac-mode" "Minor mode for advanced completion." t nil)
+(ac-mode) ;; enable
+
+(autoload 'term-toggle "~/.emacs.d/vendor/elisps/term-toggle"
+  "Toggles between the *terminal* buffer and whatever buffer you are editing." t)
+(autoload 'term-toggle-cd "~/.emacs.d/vendor/elisps/term-toggle"
+  "Pops up a shell-buffer and insert a \"cd <file-dir>\" command." t)
+(global-set-key [M-f1] 'term-toggle)
+(global-set-key (kbd "C-x t") 'term-toggle-cd)
