533059ac62d0141f9c5d863cc71bdfa5fa5fc5a2
yukihiro hara <yukihr@gmail.com>
2011-08-09 16:03:12 +0900

    ubuntu ime settings

---
+++ .emacs.d/inits/21_ime.el
@@ -0,0 +1,24 @@
+;; ibus
+(if (equal system-name "ubuntu")
+    (progn
+
+      (add-to-list 'load-path "/usr/share/emacs23/site-lisp/ibus-el")
+      (require 'ibus)
+      (add-hook 'after-init-hook 'ibus-mode-on)
+
+      ;; C-SPC は Set Mark に使う
+      (ibus-define-common-key ?\C-\s nil)
+      ;; C-/ は Undo に使う
+      (ibus-define-common-key ?\C-/ nil)
+      ;; IBusの状態によってカーソル色を変化させる
+      (setq ibus-cursor-color '("yellow" "limegreen" "limegreen"))
+      ;; C-; で半角英数モードをトグルする(iBus 側の設定も必要）
+      (ibus-define-common-key ?\C-; t)
+      ;; すべてのバッファで入力状態を共有
+      ;; (setq ibus-mode-local nil)
+      ;; isearch-mode 検索中にカーソル形状が変わるように
+      (setq ibus-isearch-cursor-type 'hollow)
+      ;; カーソル位置で予測候補ウィンドウを表示
+      (setq ibus-prediction-window-position t)
+
+      ))
