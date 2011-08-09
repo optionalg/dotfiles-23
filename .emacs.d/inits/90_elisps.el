547764b6a187b43c2a5835af1f9e1b85c0101931
yukihiro hara <yukihr@gmail.com>
2011-08-09 15:57:41 +0900

    load sequence, delete ac-mode enabling lines

--- .emacs.d/inits/90_elisps.el
+++ .emacs.d/inits/90_elisps.el
@@ -13,12 +13,15 @@
 (setq recentf-max-saved-items 1000)
 (recentf-mode 1)
 
-(autoload 'ac-mode "~/.emacs.d/vendor/elisps/ac-mode" "Minor mode for advanced completion." t nil)
-(ac-mode) ;; enable
-
 (autoload 'term-toggle "~/.emacs.d/vendor/elisps/term-toggle"
   "Toggles between the *terminal* buffer and whatever buffer you are editing." t)
 (autoload 'term-toggle-cd "~/.emacs.d/vendor/elisps/term-toggle"
   "Pops up a shell-buffer and insert a \"cd <file-dir>\" command." t)
 (global-set-key [M-f1] 'term-toggle)
 (global-set-key (kbd "C-x t") 'term-toggle-cd)
+
+;; auto-complete に同じ機能
+;; (autoload 'ac-mode "~/.emacs.d/vendor/elisps/ac-mode" "Minor mode for advanced completion." t nil)
+;; (load "~./.emacs.d/vendor/elisps/ac-mode")
+;; (load "ac-mode")
+;; (ac-mode) ;; enable
