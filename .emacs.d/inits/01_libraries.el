53a6a3170b8a68c3ad85dc8c85732f16f9b696e4
yukihiro hara <yukihr@gmail.com>
2011-10-27 14:22:51 +0900

    install cedet

--- .emacs.d/inits/01_libraries.el
+++ .emacs.d/inits/01_libraries.el
@@ -1,1 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/apel")
+
+;; disable default load-path for cedet on emacs-version > 23
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/cedet-1.0/common"))
+(load-file (expand-file-name "~/.emacs.d/vendor/cedet-1.0/common/cedet.el"))
+
