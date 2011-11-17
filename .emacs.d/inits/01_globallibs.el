9a104d1b33ad5b3b50ab9d31ccd0b7fd07c67005
yukihiro hara <yukihr@gmail.com>
2011-11-17 16:52:05 +0900

    replace library path

--- .emacs.d/inits/01_globallibs.el
+++ .emacs.d/inits/01_globallibs.el
@@ -1,8 +1,8 @@
-(add-to-list 'load-path "~/.emacs.d/vendor/apel")
+(add-to-list 'load-path "~/.emacs.d/lib/apel")
 
 ;; disable default load-path for cedet on emacs-version > 23
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/cedet-1.0/common"))
-(load-file (expand-file-name "~/.emacs.d/vendor/cedet-1.0/common/cedet.el"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lib/cedet-1.0/common"))
+(load-file (expand-file-name "~/.emacs.d/lib/cedet-1.0/common/cedet.el"))
 
 (setq semantic-load-turn-useful-things-on t)
 (unless (featurep 'cedet) (load "cedet"))
