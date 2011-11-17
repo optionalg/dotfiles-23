a566e14f6c056c09e1e1adfb0777f339ed28e268
yukihiro hara <yukihr@gmail.com>
2011-11-17 16:30:58 +0900

    imenu

--- .emacs.d/inits/46_markdown.el
+++ .emacs.d/inits/46_markdown.el
@@ -8,6 +8,15 @@
 
 (setq markdown-command "multimarkdown")
 
+(defun outline-imenu-create-index ()
+  (let (index)
+    (goto-char (point-min))
+    (while (re-search-forward "^\*\s*\\(.+\\)" (point-max) t)
+      (push (cons (match-string 1) (match-beginning 1)) index))
+    (nreverse index)))
+
+(add-hook 'markdown-mode (lambda () (setq imenu-create-index-function 'outline-imenu-create-index)))
+
 ;; (add-hook 'markdown-mode-hook
 ;;           '(lambda ()
 ;;              (refill-mode -1)
