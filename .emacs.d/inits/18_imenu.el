7e8a3b3b5626be8c0f906dc722f10e4e25150ee0
yukihiro hara <yukihr@gmail.com>
2011-11-17 16:37:23 +0900

    add imenu config

---
+++ .emacs.d/inits/18_imenu.el
@@ -0,0 +1,18 @@
+;(require 'imenu)
+(require 'semantic-imenu)
+
+(defcustom imenu-modes
+  '(emacs-lisp-mode c-mode c++-mode makefile-mode js2-mode coffee-mode)
+  "List of major modes for which Imenu mode should be used."
+  :group 'imenu
+  :type '(choice (const :tag "All modes" t)
+                 (repeat (symbol :tag "Major mode"))))
+
+;; (defun my-imenu-ff-hook ()
+;;   "File find hook for Imenu mode."
+;;   (if (member major-mode imenu-modes)
+;;       (imenu-add-to-menubar "imenu")))
+;; (add-hook 'find-file-hooks 'my-imenu-ff-hook t)
+
+;; (global-set-key "\C-cg" 'imenu)
+
