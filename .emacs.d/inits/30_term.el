af71715224f4ecebf3ffd29dba60b1ed338bdd46
yukihiro hara <yukihr@gmail.com>
2011-11-17 16:29:09 +0900

    adjust toggle action

--- .emacs.d/inits/30_term.el
+++ .emacs.d/inits/30_term.el
@@ -10,19 +10,26 @@
 ;;   * elscreen 使用時：dedicated開く → 別スクリーンへ移動 → dedicated開き閉じる → もとのスクリーンへ → dedicated 閉じる
 ;;   と新しく dedicated を開く。閉じるのが期待される挙動。
 (defun my-term-cd-cmd (dir) (concat "cd " dir "\n"))
+;; 標準の物はたまに違う値を返すので試しにこっちを使う
+;; ウィンドウの参照を中で持っているのが変な値になっているっぽい
+(defun my-multi-term-dedicated-exist-p ()
+  (if
+      (member "*MULTI-TERM-DEDICATED*"
+              (mapcar '(lambda (w) (buffer-name (window-buffer w)))
+                      (window-list))) t
+    nil))
 (global-set-key (kbd "C-x t")
                 '(lambda ()
                    (interactive)
                    (let ((cd-cmd (cond (buffer-file-name (my-term-cd-cmd (file-name-directory buffer-file-name)))
                                        (list-buffers-directory (my-term-cd-cmd list-buffers-directory))
                                        (""))))
-                     (if (and
-                          (multi-term-buffer-exist-p multi-term-dedicated-buffer)
-                          (multi-term-window-exist-p multi-term-dedicated-window))
+                     (if (my-multi-term-dedicated-exist-p)
                          (multi-term-dedicated-close)
                        (multi-term-dedicated-open)
                        (if multi-term-dedicated-select-after-open-p
                            (term-send-raw-string cd-cmd))))))
+
 (global-set-key (kbd "C-c t") 'multi-term)
 (global-set-key (kbd "C-c n") 'multi-term-next)
 (global-set-key (kbd "C-c p") 'multi-term-prev)
