fcdf22d7ec7cc8ed92dcf00abe7a0baae8e814ad
yukihiro hara <yukihr@gmail.com>
2011-11-17 22:04:30 +0900

    load emacs-deferred

--- .emacs.d/inits/01_globallibs.el
+++ .emacs.d/inits/01_globallibs.el
@@ -4,8 +4,14 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lib/cedet-1.0/common"))
 (load-file (expand-file-name "~/.emacs.d/lib/cedet-1.0/common/cedet.el"))
 
+;; semantic
 (setq semantic-load-turn-useful-things-on t)
 (unless (featurep 'cedet) (load "cedet"))
 ;; enable semantic-imenu
 (semantic-load-enable-code-helpers)
 (semantic-load-enable-minimum-features)
+
+;; deferred
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lib/emacs-deferred"))
+(load-file (expand-file-name "~/.emacs.d/lib/emacs-deferred/deferred.el"))
+(load-file (expand-file-name "~/.emacs.d/lib/emacs-deferred/concurrent.el"))
