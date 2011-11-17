dc2c5deaa6e22dd80f35fd7daf2da805265fcdbe
yukihiro hara <yukihr@gmail.com>
2011-11-17 16:26:24 +0900

    adjust auto-complete settings

--- .emacs.d/inits/11_auto-complete.el
+++ .emacs.d/inits/11_auto-complete.el
@@ -1,9 +1,20 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/auto-complete")
 
+;; for vimpulse
+;; <ESC> once to normal mode
+;; from: https://sites.google.com/site/fudist/Home/vimpulse
+(setq ac-use-quick-help nil)
+(setq ac-use-menu-map t)
+(setq ac-auto-start nil)
+(setq ac-dictionary-directories (list "~/.emacs.d/vendor/auto-complete/dict"))
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/dict")
+
+;; for vimpulse
+;; ESC が先立つキーバインドが無効になる・・
+(define-key ac-complete-mode-map "\C-[" 'viper-intercept-ESC-key)
 (ac-config-default)
 (global-auto-complete-mode t)
+(define-key ac-mode-map (kbd "TAB") 'auto-complete)
 
 (add-to-list 'ac-modes 'coffee-mode)
 (add-to-list 'ac-modes 'jade-mode)
@@ -11,4 +22,5 @@
 (add-to-list 'ac-modes 'markdown-mode)
 (add-to-list 'ac-modes 'js2-mode)
 (add-to-list 'ac-modes 'jde-mode)
+(add-to-list 'ac-modes 'nxml-mode)
 
