11e1056b2780125d7c2b6100b348d7a230f21343
yukihiro hara <yukihr@gmail.com>
2012-01-31 18:33:19 +0900

    emacs: ruby-mode

---
+++ .emacs.d/inits/51_ruby.el
@@ -0,0 +1,57 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/ruby-mode")
+(setq auto-mode-alist
+      (append '(("^Rakefile$" . ruby-mode)) auto-mode-alist))
+
+;; ruby-electric.el
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+
+;; ruby-block.el
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle 'overlay)
+
+;; inf-ruby.el
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (inf-ruby-keys)
+             ))
+
+
+;; flymake-ruby
+(defun flymake-ruby-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
+             (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+                 (flymake-mode t))
+             ))
+
+(add-hook 'ruby-mode-hook 'my-flymake-minor-mode) ;keybindings for flymake
+
+
+;; rubydb
+;; M-x rubydb
+(autoload 'rubydb "rubydb3x"
+  "run rubydb on program file in buffer *gud-file*.
+the directory containing file becomes the initial working directory
+and source-file directory for your debugger." t)
+
+;; Without this, viper-mode will be disabled in ruby-mode.
+(add-to-list 'viper-vi-state-mode-list 'ruby-mode)
