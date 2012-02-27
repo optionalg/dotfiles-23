dde85e3e77e3832681884e92ce5f112e7cf79b31
yukihiro hara <yukihr@gmail.com>
2012-02-27 17:07:46 +0900

    emacs: php-mode

---
+++ .emacs.d/inits/53_php.el
@@ -0,0 +1,10 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/php-mode-1.5.0")
+
+;; php-mode
+(require 'php-mode)
+
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(setq php-manual-path "~/.emacs.d/vendor/php-mode-1.5.0/php-manual/")
+(setq php-manual-url "http://www.php.net/manual/ja/")
+
+(setq php-mode-force-pear t)
