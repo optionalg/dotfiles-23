31fbca9b77a4e62e1d16a4e91a52ad8a7c238f69
yukihiro hara <yukihr@gmail.com>
2012-03-05 19:46:53 +0900

    emacs: woman settings

--- .emacs.d/inits/47_woman.el
+++ .emacs.d/inits/47_woman.el
@@ -5,10 +5,10 @@
 
 (setq woman-use-own-frame nil)
 (setq woman-cache-filename "~/.emacs.d/.woman-cache.el")
-;; (setq woman-imenu-generic-expression
-;;       '((nil "^\\(   \\)?\\([ぁ-んァ-ヴー一-龠ａ-ｚＡ-Ｚ０-９a-zA-Z0-9]+\\)" 2)))
+(setq woman-imenu-generic-expression
+      '((nil "^\\(   \\)?\\([ぁ-んァ-ヴー一-龠ａ-ｚＡ-Ｚ０-９a-zA-Z0-9]+\\)" 2)))
 
-(setq woman-manpath '("/usr/local/jman/share/man/ja_JP.UTF-8/" 
+(setq woman-manpath '("/usr/local/jman/share/man/ja_JP.UTF-8/"
                       "/opt/local/share/man"
                       "/usr/local/share/man"
                       "/usr/share/man"
@@ -16,17 +16,17 @@
                       ))
 
 ;; これをしないとWoManでanything-imenuしてもなにも候補がでない
-;; (setq woman-imenu t)
+(setq woman-imenu t)
 
 ;; migemoを使用したimenuソースを定義
-;; (setq anything-c-source-imenu
-;;   '((name . "Imenu")
-;;     (candidates . anything-c-imenu-candidates)
-;;     (volatile)
-;;     (persistent-action . (lambda (elm)
-;;                            (anything-c-imenu-default-action elm)
-;;                            (unless (fboundp 'semantic-imenu-tag-overlay)
-;;                              (anything-match-line-color-current-line))))
-;;     (action . anything-c-imenu-default-action)
-;;     (migemo)))
+(setq anything-c-source-imenu
+  '((name . "Imenu")
+    (candidates . anything-c-imenu-candidates)
+    (volatile)
+    (persistent-action . (lambda (elm)
+                           (anything-c-imenu-default-action elm)
+                           (unless (fboundp 'semantic-imenu-tag-overlay)
+                             (anything-match-line-color-current-line))))
+    (action . anything-c-imenu-default-action)
+    (migemo)))
 
