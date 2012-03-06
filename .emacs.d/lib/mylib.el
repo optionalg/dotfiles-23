71c295d98ba25f5e5ba5eb2c2022af30f099163e
yukihiro hara <yukihr@gmail.com>
2012-03-06 10:34:17 +0900

    emacs: update mylib.el

--- .emacs.d/lib/mylib.el
+++ .emacs.d/lib/mylib.el
@@ -5,7 +5,16 @@
 
 
 ;;; utilities ;;;
+(defun add-to-load-path (path-from-emacs-directory)
+  "Add path to load-path.
+\"path\" will be interpreted as an entry in user-emacs-directory."
+  (setq load-path
+        (cons (concat user-emacs-directory path-from-emacs-directory)
+              load-path)))
+
 (defun exec-shell-command-sync (command &rest args)
+  "Execute command in the shell, wait command to exit,
+return output of the command in string format."
   (let (ret
          (process
           (apply 'start-process-shell-command "exec" nil command args)))
@@ -18,13 +27,11 @@
       (sleep-for 0 10))
     (car ret)))
 
-
 (defun region-string-or-currnet-word ()
   "Get region string if region is set, else get current word."
   (if mark-active
       (buffer-substring (region-beginning) (region-end))
     (current-word)))
-
 
 (defun join (lst &optional delim)
   (mapconcat 'identity lst
