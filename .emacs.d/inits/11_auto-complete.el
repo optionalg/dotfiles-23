185f7ca88f6e3b14a3e313cce1a7423ee3a8a04a
yukihiro hara <yukihr@gmail.com>
2012-04-16 16:18:10 +0900

    auto-complete settings

--- .emacs.d/inits/11_auto-complete.el
+++ .emacs.d/inits/11_auto-complete.el
@@ -12,9 +12,10 @@
 ;; for vimpulse
 ;; ESC が先立つキーバインドが無効になる・・
 (define-key ac-complete-mode-map "\C-[" 'evil-esc)
-(ac-config-default)
+;(ac-config-default)
 (global-auto-complete-mode t)
-(define-key ac-mode-map (kbd "TAB") 'auto-complete)
+;(define-key ac-mode-map (kbd "TAB") 'auto-complete)
+(ac-set-trigger-key "TAB")
 
 (add-to-list 'ac-modes 'coffee-mode)
 (add-to-list 'ac-modes 'jade-mode)
@@ -23,3 +24,14 @@
 (add-to-list 'ac-modes 'js2-mode)
 (add-to-list 'ac-modes 'jde-mode)
 (add-to-list 'ac-modes 'nxml-mode)
+
+(defun my-ac-config-default ()
+  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-yasnippet ac-source-words-in-same-mode-buffers))
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (global-auto-complete-mode t))
+
+(my-ac-config-default)
