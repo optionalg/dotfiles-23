19e152997176ec17460cf528d29e57377f0836f5
yukihiro hara <yukihr@gmail.com>
2012-04-16 16:20:03 +0900

    yasnippet settings

--- .emacs.d/inits/17_yasnippet.el
+++ .emacs.d/inits/17_yasnippet.el
@@ -8,15 +8,16 @@
 ;; (setq anything-c-yas-space-match-any-greedy t)
 ;; (global-set-key (kbd "C-c y") 'anything-c-yas-complete)
 
+;Don't map TAB to yasnippet
+;In fact, set it to something we'll never use because
+;we'll only ever trigger it indirectly.
+(setq yas/trigger-key (kbd "C-c <kp-multiply>"))
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
 (yas/load-directory "~/.emacs.d/snippets")
 
 (require 'dropdown-list)
 (setq yas/prompt-functions '(yas/dropdown-prompt))
-
-;; for auto-complete
-; (require 'auto-complete-yasnippet)
 
 
 ;; from http://d.hatena.ne.jp/antipop/20080321/1206090430
