e4679c0fbc23bf642579ef58c69f800103397bc2
yukihiro hara <yukihr@gmail.com>
2013-04-05 20:11:52 +0900

    update script

--- make-homedir-symlink.sh
+++ make-homedir-symlink.sh
@@ -8,6 +8,6 @@ if [ `uname -s` = 'Darwin' ]; then
     ln -is ${file} ~/
   done
 else
-  find "${PWD}" -maxdepth 1 -name '.git' -prune -o -name '.gitignore' -prune -o -name '.*' -print0 | xargs -0 ln -is -t ~/
+  find "${PWD}" -maxdepth 1 -name '.git' -prune -o -name '.gitignore' -prune -o -name '.*' -print0 | xargs -0 ln -ins -t ~/
 fi
 
