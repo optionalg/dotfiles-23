dcd5dfe936aeca13ab7c4dae3c26eb2c9652e1b4
yukihiro hara <yukihr@gmail.com>
2013-05-14 19:43:29 +0900

    emacs/gnu global settings

--- .emacs.d/inits/25_global.el
+++ .emacs.d/inits/25_global.el
@@ -1,9 +1,19 @@
+;; Install gnu global
+;;
+;; On Mac:
+;;     $ brew install global --with-exuberant-ctags
+;;     $ brew link --overwrite ctags #if necessary, remove emacs bundled ctags
+;;
+;; Set EnVar(bash/zsh):
+;;     export GTAGSLABEL=exuberant-ctags
+
 (add-to-load-path "vendor/global")
 (require 'gtags)
 
 ;; Use with helm
 (reqpack 'helm)
 (reqpack 'helm-gtags)
+(custom-set-variables '(helm-gtags-read-only nil))
 
 ;;; Enable helm-gtags-mode
 (dolist (hook '(asm-mode-hook c-mode-common-hook))
@@ -21,3 +31,13 @@
 (define-key evil-normal-state-map (kbd "C-=") 'helm-gtags-find-rtag) ;reference
 (define-key evil-normal-state-map (kbd "C-/") 'helm-gtags-find-symbol)
 (define-key evil-normal-state-map (kbd "C-t") 'helm-gtags-pop-stack)
+
+;; update tags files on saving files
+;; http://qiita.com/items/8e8c7fca64b4810d8e78
+(defun my/update-gtags ()
+  (let* ((file (buffer-file-name (current-buffer)))
+         (dir (directory-file-name (file-name-directory file))))
+    (when (executable-find "global")
+      (start-process "gtags-update" nil
+                     "global" "-uvO"))))
+(add-hook 'after-save-hook 'my/update-gtags)
