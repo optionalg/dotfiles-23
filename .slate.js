2d5223432e91a9cc6510e8011b0cc8ab24792dde
yukihiro hara <yukihr@gmail.com>
2013-06-02 03:10:24 +0900

    slate/update js

--- .slate.js
+++ .slate.js
@@ -1,26 +1,34 @@
-var launch_and_focus = function (target) {
-  return function (win) {
-    if (!win) { return; }
-    if (!(function(){
-      try {
-        S.eachApp(function(app){ if(app.name() === target){throw {};}});
-      } catch(e) {
-        return true;
-      }
-      return false;
-    })()) {
-      win.doOperation(
-        S.operation('shell', {
-          "command": "/usr/bin/open -a " + target,
-          "wait": "waitForExit"
-        })
-      );
+var isAppLaunched = function(name) {
+  return (function(){
+    try {
+      S.eachApp(function(app){ if(app.name() === name){throw {};}});
+    } catch(e) {
+      return true;
     }
-    win.doOperation(S.operation('focus', { "app": target }));
+    return false;
+  })();
+};
+
+var launchApp = function(win, name) {
+  return win.doOperation(
+    S.operation('shell', {
+      "command": "/usr/bin/open -a " + name,
+      "wait": "waitForExit"
+    })
+  );
+};
+
+var _to = function(name) {
+  return function (win) {
+    if (!win) { win = S.windowUnderPoint({'x':100,'y':100}); }
+    if (!isAppLaunched(name)) {
+      launchApp(win, name)
+    }
+    win.doOperation(S.operation('focus', { "app": name }));
   };
 };
 
-S.bind('1:cmd', launch_and_focus('Safari'));
-S.bind('2:cmd', launch_and_focus('Emacs'));
-S.bind('3:cmd', launch_and_focus('Dash'));
-S.bind('4:cmd', launch_and_focus('Chromium'));
+S.bind('1:cmd', _to('Safari'));
+S.bind('2:cmd', _to('Emacs'));
+S.bind('3:cmd', _to('Dash'));
+S.bind('4:cmd', _to('Chromium'));
