ce808ac20f6b2ddd2313d0f44897840549338d06
yukihiro hara <yukihr@gmail.com>
2013-06-04 18:53:38 +0900

    emacs/coffeescript flymake

--- .emacs.d/inits/40_coffee.el
+++ .emacs.d/inits/40_coffee.el
@@ -13,42 +13,43 @@
 
 (define-key coffee-mode-map [(super r)] 'coffee-compile-buffer)
 
-;; flymake for coffee script, form: http://d.hatena.ne.jp/antipop/20110508/1304838383
-(setq flymake-coffeescript-err-line-patterns
-      '())
+;; ;; flymake for coffee script, form: http://d.hatena.ne.jp/antipop/20110508/1304838383
+;; (setq flymake-coffeescript-err-line-patterns
+;;       '())
 
-(defconst flymake-allowed-coffeescript-file-name-masks
-  '(("\\.coffee$" flymake-coffeescript-init)))
+;; (defconst flymake-allowed-coffeescript-file-name-masks
+;;   '(("\\.coffee$" flymake-coffeescript-init)))
 
-(defun flymake-coffeescript-init ()
-  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-         (local-file  (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list "coffee" (list
-                                        ;"--compile"
-                                        ;"--output" local-dir
-                    local-file))) ;; add "--bare" to access inner objs globally
-  )
+;; (defun flymake-coffeescript-init ()
+;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;          (local-file  (file-relative-name
+;;                        temp-file
+;;                        (file-name-directory buffer-file-name))))
+;;     (list "coffee" (list
+;;                                         ;"--compile"
+;;                                         ;"--output" local-dir
+;;                     local-file))) ;; add "--bare" to access inner objs globally
+;;   )
 
-(defun flymake-coffeescript-load ()
-  (interactive)
-  (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
-    (setq flymake-check-was-interrupted t))
-  (ad-activate 'flymake-post-syntax-check)
-  (setq flymake-allowed-file-name-masks
-        (append flymake-allowed-file-name-masks
-                flymake-allowed-coffeescript-file-name-masks))
-  (setq flymake-err-line-patterns (cons
-                                   '("\\(Error: In \\([^,]+\\), .+ on line \\([0-9]+\\).*\\)" 2 3 nil 1)
-                                   flymake-err-line-patterns))
-  (flymake-mode t))
+;; (defun flymake-coffeescript-load ()
+;;   (interactive)
+;;   (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+;;     (setq flymake-check-was-interrupted t))
+;;   (ad-activate 'flymake-post-syntax-check)
+;;   (setq flymake-allowed-file-name-masks
+;;         (append flymake-allowed-file-name-masks
+;;                 flymake-allowed-coffeescript-file-name-masks))
+;;   (setq flymake-err-line-patterns (cons
+;;                                    '("\\(Error: In \\([^,]+\\), .+ on line \\([0-9]+\\).*\\)" 2 3 nil 1)
+;;                                    flymake-err-line-patterns))
+;;   (flymake-mode t))
 
-(add-hook 'coffee-mode-hook 'flymake-coffeescript-load)
+;; (add-hook 'coffee-mode-hook 'flymake-coffeescript-load)
 
-(add-hook 'coffee-mode-hook 'my-flymake-minor-mode) ;keybindings for flymake
-
+;; (add-hook 'coffee-mode-hook 'my-flymake-minor-mode) ;keybindings for flymake
+(reqpac 'flymake-coffee)
+(add-hook 'coffee-mode-hook 'flymake-coffee-load)
 
 ;; funcs
 ;; using coffee-command in vender/coffee-mode/coffee-mode.el
