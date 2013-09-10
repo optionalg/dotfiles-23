2c726cd5d5938c33991b35ca78e508227f8d96ba
yukihiro hara <yukihr@gmail.com>
2013-09-11 01:37:43 +0900

    emacs/add magit-flow

--- .emacs.d/inits/16_magit.el
+++ .emacs.d/inits/16_magit.el
@@ -1,6 +1,7 @@
 (reqpac 'magit)
 (define-key global-map [(super g)] 'magit-status)
 
+(add-to-load-path "vendor/magit-plugins")
 (require 'magit-flow)
 
 (add-hook 'magit-mode-hook 'turn-on-magit-flow)
