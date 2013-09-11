36f80e7ec3a2f4d7adc767855ae68d66aee9ef0b
yukihiro hara <yukihr@gmail.com>
2013-09-11 11:03:18 +0900

    emacs/enable web mode on *.erb, not on *.md.erb

--- .emacs.d/inits/46_web.el
+++ .emacs.d/inits/46_web.el
@@ -3,9 +3,7 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.ctp\\'"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . web-mode))
-
-
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
 ;; web-modeの設定
 (defun web-mode-hook ()
