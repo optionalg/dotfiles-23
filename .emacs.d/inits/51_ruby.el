cfca2360d0349a6f6e7ae3d0e34d2d8c1c024c5f
yukihiro hara <yukihr@gmail.com>
2013-11-05 15:12:32 +0900

    emacs/install enhanced-ruby-mode but not usinng

--- .emacs.d/inits/51_ruby.el
+++ .emacs.d/inits/51_ruby.el
@@ -3,6 +3,24 @@
                 ("Gemfile$" . ruby-mode)
                 ("Rakefile$" . ruby-mode)) auto-mode-alist))
 
+;; Enhanced ruby mode
+;; (add-to-load-path "vendor/enhanced-ruby-mode")
+;; (setq enh-ruby-program 
+;;       (replace-regexp-in-string
+;;        "\n+$" "" (shell-command-to-string "rbenv which ruby")))
+;; (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+;; (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+;; (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+;; (add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+;; (add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+;; (add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
+;; (add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+ 
+;; (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+ 
+;; (setq enh-ruby-bounce-deep-indent t)
+;; (setq enh-ruby-hanging-brace-indent-level 2)
+ 
 ;; rbenv
 (setq rbenv-executable "/usr/local/bin/rbenv")
 (setq rbenv-show-active-ruby-in-modeline nil)
