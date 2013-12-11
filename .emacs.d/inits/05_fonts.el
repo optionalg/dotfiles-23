5dec1ba4207b19826356b81f45600530ebbc8ebf
yukihiro hara <yukihr@gmail.com>
2013-12-11 19:48:13 +0900

    emacs/font

--- .emacs.d/inits/05_fonts.el
+++ .emacs.d/inits/05_fonts.el
@@ -20,14 +20,14 @@
   (set-fontset-font
    (frame-parameter nil 'font)
    'mule-unicode-0100-24ff
-   '("monaco" . "iso10646-1"))
+   '("menlo" . "iso10646-1"))
   (setq face-font-rescale-alist
         '(("^-apple-hiragino.*" . 1.1)
           (".*osaka-bold.*" . 1.2)
           (".*osaka-medium.*" . 1.2)
           (".*courier-bold-.*-mac-roman" . 1.0)
-          (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-          (".*monaco-bold-.*-mac-roman" . 0.9)
+          (".*menlo cy-bold-.*-mac-cyrillic" . 0.9)
+          (".*menlo-bold-.*-mac-roman" . 0.9)
           ("-cdac$" . 1.3))))
 
 ;; Ubuntu
