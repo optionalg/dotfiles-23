ac544f47c0ee2becfa4b68d0edfdf21c6b47b4a9
yukihiro hara <yukihr@gmail.com>
2013-12-18 22:41:15 +0900

    emacs/eldoc idle time

--- .emacs.d/inits/59_el.el
+++ .emacs.d/inits/59_el.el
@@ -1,6 +1,6 @@
 (require 'eldoc)
 (require 'eldoc-extension)
-(setq eldoc-idle-delay 0)
+(setq eldoc-idle-delay 0.3)
 (setq eldoc-echo-area-use-multiline-p t)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
