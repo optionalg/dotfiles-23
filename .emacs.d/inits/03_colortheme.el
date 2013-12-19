1e95865d58d12a7e655e176a72149b11cd96aaa9
yukihiro hara <yukihr@gmail.com>
2013-12-19 22:56:45 +0900

    update

--- .emacs.d/inits/03_colortheme.el
+++ .emacs.d/inits/03_colortheme.el
@@ -17,9 +17,11 @@
 ;; Buffer specific bg-color
 ;; http://www.emacswiki.org/emacs/BufferBackgroundColor
 (defvar chosig-alist
-  '((term-mode . "#0f0f0f"))
-    ;; ("scratch" . "#fee")
-    ;; (emacs-lisp-mode . "#eee"))
+  '(
+    (term-mode . "#0f0f0f")
+    )
+  ;; ("scratch" . "#fee")
+  ;; (emacs-lisp-mode . "#eee"))
   "Alist matching major modes or buffer names with background colors.
 Every cons cell on the alist has the form (CHECK . COLOR) where CHECK
 is either a symbol matching the `major-mode' or a regular expression
@@ -30,23 +32,23 @@ representing a valid color, eg. \"red\" or \"f00\".")
   "Pick background-color according to `chosig-alist'.
 The overlay used is stored in `chosig-background'."
   (let ((alist chosig-alist)
-	background)
+        background)
     (while (and (not background) alist)
       (if (or (and (stringp (caar alist))
-		   (string-match (caar alist) (buffer-name)))
-	      (eq major-mode (caar alist)))
-	  (setq background (cdar alist))
-	(setq alist (cdr alist))))
+                   (string-match (caar alist) (buffer-name)))
+              (eq major-mode (caar alist)))
+          (setq background (cdar alist))
+        (setq alist (cdr alist))))
     ;; cleanup
     (mapc (lambda (o)
-	    (when (overlay-get o 'chosig)
-	      (delete-overlay o)))
-	  (overlays-in (point-min) (point-max)))
+            (when (overlay-get o 'chosig)
+              (delete-overlay o)))
+          (overlays-in (point-min) (point-max)))
     ;; new one
     (when background
       (let ((o (make-overlay (point-min) (point-max)
-			     (current-buffer) nil t)))
-	(overlay-put o 'face `(:background ,background))
-	(overlay-put o 'chosig t)))))
+                             (current-buffer) nil t)))
+        (overlay-put o 'face `(:background ,background))
+        (overlay-put o 'chosig t)))))
 
 (add-hook'after-change-major-mode-hook 'chosig-choose-background)
