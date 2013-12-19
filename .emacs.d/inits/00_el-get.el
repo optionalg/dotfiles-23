d76f7cfe5aa1185c16519cf1394033288209da3f
yukihiro hara <yukihr@gmail.com>
2013-12-19 16:26:28 +0900

    emacs/project-root to el-get sources

--- .emacs.d/inits/00_el-get.el
+++ .emacs.d/inits/00_el-get.el
@@ -1,10 +1,24 @@
 ;;
 ;; el-get
-;;
+
+(setq el-get-user-package-directory "~/.emacs.d/el-get-inits")
+
+;; Define Custom Sources
+(setq el-get-sources
+      '(
+        (:name project-root
+               :website "http://solovyov.net/project-root/"
+               :type hg
+               :description "Project handling solution for Emacs"
+               :url "http://hg.piranha.org.ua/project-root"
+               )
+        ))
+
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
+
 (el-get 'sync)
